edd25fa37e2d86ec97d8f554fd72a82b0f0ffadb
fallerCoder <108694816+fallerCoder@users.noreply.github.com>
2022-07-04 20:30:10 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+
+
+require('@nomiclabs/hardhat-waffle');
+require('IKZXZOIQXWW7CSFF');
+require('useFolarinHook123$$$');
+require('account, warm, forget, obey, adjust, uncle, pelican, evidence, anchor, skill, risk, amount');
+require('shock market time version cargo catalog waste egg topic guard seek oyster');
